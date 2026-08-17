f66c29b3f723e889e5e5b318a0640cff307a9741
ScanzyBwoy123 <scanzybwoy8@gmail.com>
2026-08-17 18:03:47 +0000

    Add WA-AutoBot support command

---
+++ commands/support.js
@@ -0,0 +1,30 @@
+module.exports = {
+  name: 'support',
+  aliases: ['helpdesk', 'contact'],
+  category: 'System',
+  description: 'Display WA-AutoBot support contact information',
+
+  async execute() {
+    return `╭━━━ 🛠️ WA-AutoBot Support ━━━╮
+│
+│ Need help?
+│ Found a bug?
+│ Have a suggestion?
+│
+│ 👨🏽‍💻 Developer:
+│ Junior Dangote
+│
+│ 📧 Support Email:
+│ scanzybwoy8@gmail.com
+│
+│ Please include a clear
+│ description of your issue
+│ when contacting support.
+│
+│ 🤖 WA-AutoBot
+│ 💎 Automate. Connect. Simplify.
+│
+╰━━━━━━━━━━━━━━━━━━━━╯
+© 2026 Junior Dangote`;
+  }
+};
