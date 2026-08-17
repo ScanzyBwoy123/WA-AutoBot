0b54ea31a763cb600645ff85b14c31e57369b6d8
ScanzyBwoy123 <scanzybwoy8@gmail.com>
2026-08-17 18:01:30 +0000

    Add owner information command

---
+++ commands/owner.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: 'owner',
+  aliases: ['creator', 'developer'],
+  category: 'System',
+  description: 'Display the bot owner and creator information',
+
+  async execute() {
+    return `╭━━━ 👑 WA-AutoBot ━━━╮
+│
+│ 👨🏽‍💻 Founder & Developer
+│ Junior Dangote
+│
+│ 🤖 Bot: WA-AutoBot
+│ 📌 Version: 1.0.0
+│
+│ 💎 Motto:
+│ Automate. Connect. Simplify.
+│
+│ 🛠️ Need help?
+│ Use .support
+│
+╰━━━━━━━━━━━━━━━━━━━━╯
+© 2026 Junior Dangote`;
+  }
+};
